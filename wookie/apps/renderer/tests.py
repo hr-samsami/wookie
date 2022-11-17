96518ab965db1746a3e6811ce6bb9ebd6ebbc79a
h.samsami <hamidreza.samsami@gmail.com>
2022-11-17 20:31:30 +0330

    Add Tests for the renderer api

--- wookie/apps/renderer/tests.py
+++ wookie/apps/renderer/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class RendererTests(APITestCase):
+    def setUp(self):
+        self.json_type = 'application/json'
+        self.xml_type = 'application/xml'
+        self.url = reverse('renderer')
+
+    def test_response_default_content_type_is_json(self):
+        """
+        Ensure default Content-Type for non XML type is JSON.
+        """
+        sample_types = ['text/css', 'image/gif', 'text/html']
+        for ct in sample_types:
+            resp = self.client.get(self.url, content_type=ct)
+            self.assertEqual(resp.status_code, status.HTTP_200_OK)
+            self.assertTrue('application/json' in resp['Content-Type'])
+
+    def test_json_response_when_content_type_is_json(self):
+        """
+        Ensure when Content-Type in the header request is application/json then the response is JSON too.
+        """
+        resp = self.client.get(self.url, content_type=self.json_type)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertTrue(self.json_type in resp['Content-Type'])
+
+    def test_xml_response_when_content_type_is_xml(self):
+        """
+        Ensure when Content-Type in the header request is application/xml then the response is XML too.
+        """
+        resp = self.client.get(self.url, content_type=self.xml_type)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertTrue(self.xml_type in resp['Content-Type'])
--- wookie/apps/renderer/url.py
+++ wookie/apps/renderer/url.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.RendererView.as_view())
+    path('', views.RendererView.as_view(), name='renderer')
 ]
