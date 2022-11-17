f2b7c628d5e4a2182ebea93a16033d92476776e3
h.samsami <hamidreza.samsami@gmail.com>
2022-11-17 16:09:03 +0330

    feat: Add rest_framework_xml to requirements - it's necessary for rendering requests in XML format

--- wookie/apps/renderer/apps.py
+++ wookie/apps/renderer/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RendererConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'renderer'
+    name = 'wookie.apps.renderer'
