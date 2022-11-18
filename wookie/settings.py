2bc10c5b0e181e0aff980145f307c76822d4122e
h.samsami <hamidreza.samsami@gmail.com>
2022-11-18 19:27:45 +0330

    feat: Initial Book App

--- wookie/settings.py
+++ wookie/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'wookie.apps.renderer',
-    'wookie.apps.author'
+    'wookie.apps.author',
+    'wookie.apps.book'
 ]
 
 MIDDLEWARE = [
