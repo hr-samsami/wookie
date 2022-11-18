eef94450796592faf1838a090db4553a7693a9a0
h.samsami <hamidreza.samsami@gmail.com>
2022-11-18 18:20:58 +0330

    Add Additional Tests For The Author Model

--- wookie/apps/author/tests/test_model.py
+++ wookie/apps/author/tests/test_model.py
@@ -5,9 +5,40 @@ USER_MODEL = get_user_model()
 
 
 class AuthorTests(APITestCase):
+    def setUp(self):
+        self.username = 'efaz'
+        self.email = 'algho@gmail.com'
+        self.pseudonym = 'sara'
+        self.password = 'password'
+
     def test_creates_author(self):
-        pseudonym = 'sara'
-        author = USER_MODEL.objects.create_user('efaz', pseudonym=pseudonym, email='algho@gmail.com',
-                                                password='password')
+        author = USER_MODEL.objects.create_user(username=self.username,
+                                                pseudonym=self.pseudonym,
+                                                email=self.email,
+                                                password=self.password)
         self.assertIsInstance(author, USER_MODEL)
-        self.assertEqual(author.pseudonym, pseudonym)
+        self.assertFalse(author.is_staff)
+        self.assertEqual(author.pseudonym, self.pseudonym)
+
+    def test_creates_super_user(self):
+        author = USER_MODEL.objects.create_superuser(username=self.username,
+                                                     pseudonym=self.pseudonym,
+                                                     email=self.email,
+                                                     password=self.password)
+        self.assertIsInstance(author, USER_MODEL)
+        self.assertTrue(author.is_staff)
+        self.assertEqual(author.pseudonym, self.pseudonym)
+
+    def test_raises_error_when_no_username_is_supplied(self):
+        self.assertRaises(TypeError, USER_MODEL.objects.create_user,
+                          pseudonym=self.pseudonym,
+                          email=self.email,
+                          password=self.password)
+
+    def test_default_pseudonym_value_is_empty(self):
+        author = USER_MODEL.objects.create_user(username=self.username,
+                                                email=self.email,
+                                                password=self.password)
+        self.assertIsInstance(author, USER_MODEL)
+        self.assertFalse(author.is_staff)
+        self.assertEqual(author.pseudonym, None)
