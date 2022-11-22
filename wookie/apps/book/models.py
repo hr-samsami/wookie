8c9f911bc20f89da99c6ae6220fd41df71803460
h.samsami <hamidreza.samsami@gmail.com>
2022-11-22 08:39:08 +0330

    feat: Create a Book model

--- wookie/apps/book/models.py
+++ wookie/apps/book/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import validate_image_file_extension
 from django.db import models
 
-# Create your models here.
+USER_MODEL = get_user_model()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255, null=False)
+    description = models.TextField(blank=False, null=False)
+    cover_image = models.ImageField(upload_to='books/covers', verbose_name='cover image', max_length=500,
+                                    validators=[validate_image_file_extension],
+                                    error_messages={'invalid_extension': '%(value)s'})
+    author = models.OneToOneField(USER_MODEL, on_delete=models.CASCADE)
