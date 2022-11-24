1c9fd14d7ccf2d607204e674a73013c08fd94241
h.samsami <hamidreza.samsami@gmail.com>
2022-11-24 14:33:01 +0330

    Rename url.py to urls.py of renderer

--- wookie/urls.py
+++ wookie/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('renderer/', include('wookie.apps.renderer.url'))
+    path('renderer/', include('wookie.apps.renderer.urls')),
 ]
