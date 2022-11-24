72816d5f190c9eb9c4f3b2504f64adab18d6321d
h.samsami <hamidreza.samsami@gmail.com>
2022-11-24 14:26:51 +0330

    Add `author_name` property to Author model

--- wookie/apps/book/models.py
+++ wookie/apps/book/models.py
@@ -11,10 +11,14 @@ class Book(models.Model):
     description = models.TextField(blank=False, null=False)
     cover_image = models.ImageField(upload_to='images/book-covers', verbose_name='cover image', max_length=500,
                                     validators=[validate_image_file_extension],
-                                    error_messages={'invalid_extension': '%(value)s'})
+                                    error_messages={'invalid_extension': '%(value)s'}, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def author_name(self):
+        return self.author.name
 
     def __str__(self):
         return self.title
