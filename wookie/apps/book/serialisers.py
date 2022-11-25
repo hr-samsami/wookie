735f1431dbbd9478b7c479ab364d9c7a092baf43
h.samsami <hamidreza.samsami@gmail.com>
2022-11-25 21:34:15 +0330

    feat: Add BookAddView to create Book

--- wookie/apps/book/serialisers.py
+++ wookie/apps/book/serialisers.py
@@ -5,7 +5,7 @@ from wookie.apps.book.models import Book
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['id', 'author', 'title', 'description', 'author_name', 'cover_image', 'price']
+        fields = ['id', 'title', 'description', 'author_name', 'cover_image', 'price']
         extra_kwargs = {
-            'author': {'write_only': True}
+            'author_name': {'read_only': True}
         }
