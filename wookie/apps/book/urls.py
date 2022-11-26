ae03068aa2cb3ffcfdf1923cea6b87b163af194f
h.samsami <hamidreza.samsami@gmail.com>
2022-11-26 09:09:09 +0330

    Add simple CRUD for Book resource

--- wookie/apps/book/urls.py
+++ wookie/apps/book/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import BookListView, BookAddView
+from . import views
+from .views import BookListView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book-list'),
-    path('add/', BookAddView.as_view(), name='book-add')
+    path('mylist/', views.my_books, name='book-mylist'),
+    path('detail/<int:pk>/', views.book_detail, name='book-detail'),
+    path('create/', views.book_create, name='book-create'),
+    path('update/<int:pk>/', views.book_update, name='book-update'),
+    path('delete/<int:pk>/', views.book_delete, name='book-delete'),
 ]
--- wookie/apps/book/views.py
+++ wookie/apps/book/views.py
@@ -1,6 +1,9 @@
 from django_filters import rest_framework as filters
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from wookie.apps.book.filters import BookFilter
 from wookie.apps.book.models import Book
@@ -21,4 +24,54 @@ class BookListView(generics.ListAPIView):
     serializer_class = BookSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_class = BookFilter
+
+
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def my_books(request):
+    books = Book.objects.prefetch_related('author').filter(author=request.user).all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def book_detail(request, pk):
+    book = Book.objects.prefetch_related('author').filter(author=request.user).filter(id=pk).first()
+    serializer = BookSerializer(book, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def book_create(request):
+    serializer = BookSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save(author=request.user)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['PUT'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def book_update(request, pk):
+    book = Book.objects.prefetch_related('author').filter(author=request.user).filter(id=pk).first()
+    serializer = BookSerializer(instance=book, data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save(author=request.user)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['DELETE'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
+def book_delete(request, pk):
+    book = Book.objects.prefetch_related('author').filter(author=request.user).filter(id=pk).get()
+    book.delete()
+    return Response('Deleted', status=status.HTTP_200_OK)
+
+
 
