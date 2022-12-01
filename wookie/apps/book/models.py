5842074d41dea71986a0fa74f133c957da185a9f
h.samsami <hamidreza.samsami@gmail.com>
2022-12-01 15:21:41 +0330

    Add min lenght validator to title and description of Book

--- wookie/apps/book/models.py
+++ wookie/apps/book/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import validate_image_file_extension
+from django.core.validators import validate_image_file_extension, MinLengthValidator
 from django.db import models
 
 USER_MODEL = get_user_model()
@@ -7,13 +7,13 @@ USER_MODEL = get_user_model()
 
 class Book(models.Model):
     author = models.ForeignKey(USER_MODEL, on_delete=models.DO_NOTHING)
-    title = models.CharField(max_length=255, null=False, db_index=True)
-    description = models.TextField(blank=False, null=False)
+    title = models.CharField(max_length=255, db_index=True, validators=[MinLengthValidator(3)])
+    description = models.TextField(validators=[MinLengthValidator(3)])
     cover_image = models.ImageField(upload_to='images/book-covers', verbose_name='cover image', max_length=500,
                                     validators=[validate_image_file_extension],
                                     error_messages={'invalid_extension': '%(value)s'}, blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
-    published = models.BooleanField(blank=False, null=True, default=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    published = models.BooleanField(blank=True, default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
